62d2ff197cba0b7610876e3a028d7ae480420f43
Viktor Lopatin <v.lopatin@hh.ru>
2015-10-10 15:55:53 +0300

    SERVER base_data table

---
+++ src/main/java/ru/pihta/nocturnaltransport/controller/TestController.java
@@ -0,0 +1,15 @@
+package ru.pihta.nocturnaltransport.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String test() {
+        return "none";
+    }
+}
--- src/main/java/ru/pihta/nocturnaltransport/model/Station.java
+++ src/main/java/ru/pihta/nocturnaltransport/model/Station.java
@@ -1,7 +1,21 @@
 package ru.pihta.nocturnaltransport.model;
 
-/**
- * Created by Lev on 10.10.2015.
- */
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Station")
 public class Station {
+
+//    "ID" integer NOT NULL,
+//            "Name" character varying(64) NOT NULL,
+//    "LineID" integer NOT NULL,
+//            "Latitude" double precision,
+//    "Longitude" double precision,
+//    "OpenTime" time with time zone NOT NULL,
+//    "CloseTime" time with time zone NOT NULL,
+//    "TypeID" integer NOT NULL
+
+    private
+
 }
