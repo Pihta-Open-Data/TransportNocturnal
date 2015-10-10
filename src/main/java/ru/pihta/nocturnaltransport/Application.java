6c7dd78b7b64f86e94a9b6a34a7b91f9b342484d
Viktor Lopatin <v.lopatin@hh.ru>
2015-10-10 03:05:01 +0300

    SERVER update helloworld with static autoreload

--- src/main/java/ru/pihta/nocturnaltransport/Application.java
+++ src/main/java/ru/pihta/nocturnaltransport/Application.java
@@ -3,10 +3,25 @@ package ru.pihta.nocturnaltransport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public TemplateResolver templateResolver(){
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("WEB_INF/classes/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCacheable(false);
+        templateResolver.setCacheTTLMs(0L);
+
+        return templateResolver;
     }
 }
--- src/main/java/ru/pihta/nocturnaltransport/controller/HelloController.java
+++ src/main/java/ru/pihta/nocturnaltransport/controller/HelloController.java
@@ -1,16 +1,16 @@
 package ru.pihta.nocturnaltransport.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/hello")
 public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String hello(){
-        return "Hello world!";
+        return "hello";
     }
 
 
