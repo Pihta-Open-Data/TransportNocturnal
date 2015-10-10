b8c20f7d923a8ea1d0b98306a8e72a3c226bd989
zverev <alexander.zverev@gmail.com>
2015-10-10 18:23:09 +0300

    CLIENT include Backbone

--- src/main/webapp/app/main.js
+++ src/main/webapp/app/main.js
@@ -1,8 +1,18 @@
 requirejs.config({
     paths: {
-        'leaflet': 'lib/leaflet/dist/leaflet-src'
+        'leaflet': 'lib/leaflet/dist/leaflet-src',
+        'underscore': 'lib/underscore/underscore',
+        'backbone': 'lib/backbone/backbone',
+        'jquery': 'lib/jquery/dist/jquery'
     },
     shim: {
+        'underscore': {
+            exports: '_'
+        },
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
         'lib/leaflet.maskcanvas/src/QuadTree': {
             exports: 'QuadTree'
         },
