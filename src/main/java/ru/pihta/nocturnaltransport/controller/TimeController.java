fadec3f0dec49296f886de92605b7b8797c70ae1
LeoOsipovHSE <osipov.enterprise@gmail.com>
2015-10-11 09:26:22 +0300

    time controller

--- src/main/java/ru/pihta/nocturnaltransport/controller/TimeController.java
+++ src/main/java/ru/pihta/nocturnaltransport/controller/TimeController.java
@@ -1,22 +1,19 @@
 package ru.pihta.nocturnaltransport.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import ru.pihta.nocturnaltransport.model.Service.StationWayService;
 import ru.pihta.nocturnaltransport.model.StationWay;
 
-@RestController
-@RequestMapping("/test")
-public class TestController {
+import java.time.LocalTime;
 
-    @Autowired
-    StationWayService stationWayService;
+@RestController
+@RequestMapping("/getTime")
+public class TimeController {
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String test() {
-        return stationWayService.getById(0).getFirstTrainEven().toString();
+        return LocalTime.now().toString();
     }
 }
