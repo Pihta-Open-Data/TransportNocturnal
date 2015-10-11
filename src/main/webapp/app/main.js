43eb693a1c56206ca1ac1f5abe4e0e3204c8b903
zverev <alexander.zverev@gmail.com>
2015-10-11 14:34:02 +0300

    CLIENT forOfWar layer

--- src/main/webapp/app/main.js
+++ src/main/webapp/app/main.js
@@ -5,7 +5,9 @@ requirejs.config({
         'leaflet': 'lib/leaflet/dist/leaflet-src',
         'underscore': 'lib/underscore/underscore',
         'backbone': 'lib/backbone/backbone',
-        'jquery': 'lib/jquery/dist/jquery'
+        'jquery': 'lib/jquery/dist/jquery',
+        'quadTree': 'lib/leaflet.maskcanvas/src/QuadTree',
+        'maskcanvas': 'lib/leaflet.maskcanvas/src/L.TileLayer.MaskCanvas'
     },
     shim: {
         'underscore': {
@@ -15,11 +17,11 @@ requirejs.config({
             deps: ['underscore', 'jquery'],
             exports: 'Backbone'
         },
-        'lib/leaflet.maskcanvas/src/QuadTree': {
+        'quadTree': {
             exports: 'QuadTree'
         },
-        'lib/leaflet.maskcanvas/src/L.TileLayer.MaskCanvas': {
-            deps: ['lib/leaflet/dist/leaflet-src', 'lib/leaflet.maskcanvas/src/QuadTree'],
+        'maskcanvas': {
+            deps: ['leaflet', 'quadTree'],
             exports: 'L.TileLayer.MaskCanvas'
         }
     }
@@ -27,21 +29,44 @@ requirejs.config({
 
 define([
     'map',
+    'config',
+    'models/stationsCollection',
     'layers/stationsLayer',
     'layers/totalLinesLayer',
+    'models/totalLinesCollection',
     'modalsManager',
     'views/DepartureDialog',
-    'models/stationsCollection'
+    'models/stationsCollection',
+    'layers/FogOfWarLayer',
+    'layers/LinesLayer',
+    'models/linesCollectionFactory'
 ], function(
     map,
+    config,
+    stationsCollection,
     stationsLayer,
     totalLinesLayer,
+    totalLinesCollection,
     modalsManager,
     DepartureDialog,
-    stationsCollection
+    stationsCollection,
+    FogOfWarLayer,
+    LineLayer,
+    linesCollectionFactory
 ) {
     map.addLayer(stationsLayer);
     map.addLayer(totalLinesLayer);
+
+    var fogOfWarLayer = new FogOfWarLayer({
+        radius: 700,
+        zIndex: 100000,
+        lineDelta: 0.001,
+        linesCollection: totalLinesCollection,
+        stationsCollection: stationsCollection
+    });
+    window.map = map;
+    window.fwl = fogOfWarLayer;
+    fogOfWarLayer.addTo(map);
 
     stationsLayer.on('stationclick', function(le) {
         var departureDialog = new DepartureDialog({
@@ -49,15 +74,25 @@ define([
                 id: le.stationId
             })
         });
-        departureDialog.on('confirm', function() {
-            modalsManager.destroyModal();
+        departureDialog.on('confirm', function(model, hours, minutes) {
+            linesCollectionFactory($.ajax(config.serverRoot + 'lines?stationId=' + model.get('id') + '&hours=' + hours + '&minutes=' + minutes)).then(function(collection) {
+                map.removeLayer(totalLinesCollection);
+                map.addLayer(new LineLayer({
+                    collection: collection,
+                    stationsCollection: stationsCollection
+                }))
+                modalsManager.destroyModal();
+            }).fail(function(err) {
+                console.log('fail ' + err);
+            });
+
         });
         departureDialog.on('cancel', function() {
             modalsManager.destroyModal();
         });
         modalsManager.setModal(departureDialog);
     });
-    
+
     map.on('click', function() {
         modalsManager.destroyModal();
     });
